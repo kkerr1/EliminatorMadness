a470a7afec92a7e4a79e75f24db15743c64b08c3
Kevin Kerr <kkerr1@gmail.com>
2016-01-16 19:44:33 -0800

    get redux and redux router going

---
+++ client/src/containers/app.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { routeActions } from 'redux-simple-router';
+import Radium from 'radium';
+
+@Radium
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+
+          <Link to="/">Home</Link>
+          {' | '}
+          <Link to="/standings">Standings</Link>
+        </header>
+        <div style={[{marginTop: '1.5em'}]}>{this.props.children}</div>
+      </div>
+    );
+  }
+}
+export default connect(null, { push: routeActions.push })(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,36 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import { syncHistory, routeReducer } from 'redux-simple-router';
+import App from './containers/app';
+import Home from './containers/home';
+import Standings from './containers/standings';
 
-render(<App />, document.getElementById('root'));
+import createHistory from 'history/lib/createHashHistory';
+
+const reducer = combineReducers(Object.assign({}, {
+  routing: routeReducer
+}));
+const browserHistory = createHistory();
+// Sync dispatched route actions to the history
+const reduxRouterMiddleware = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+// Required for replaying actions from devtools to work
+reduxRouterMiddleware.listenForReplays(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+       <Route path="standings" component={Standings} />
+       <IndexRoute component={Home}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
