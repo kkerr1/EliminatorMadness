8e78991011f65fb2ac409d7510b1ec8c07df1fe3
Kevin Kerr <kkerr1@gmail.com>
2016-01-16 17:57:41 -0800

    get radium going.

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 
+const styles = {
+  base: {
+    background: 'blue',
+    border: 0,
+    borderRadius: 4,
+    color: 'white',
+    padding: '1.5em'
+  },
 
+  block: {
+    display: 'block'
+  }
+};
+
+@Radium
 export class App extends Component {
   render() {
     return (
-      <div>
-      Hi!
+      <div style={[styles.base]}>
+      Hi! there
       </div>
     );
   }
