78b61e075ad8b7602a8b15428570dd643b1f7d1d
Kevin Kerr <kkerr1@gmail.com>
2016-01-25 20:49:53 -0800

    Initial dates

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,29 @@
 'use strict';
-var router = require('express').Router;
-
+/* eslint-disable */
+var router = require('express').Router();
+/* eslint-enable */
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
+  console.log('Request: ', req.url);
   next();
 });
 // define the home page route
 router.get('/dates', function (req, res) {
-  res.send({time: Date.now()});
+
+  var dates = [
+    {date: '2015-03-17', title: 'Round of 64: Day 1', teams: 2},
+    {date: '2015-03-18', title: 'Round of 64: Day 2', teams: 2},
+    {date: '2015-03-19', title: 'Round of 32: Day 1', teams: 1},
+    {date: '2015-03-20', title: 'Round of 32: Day 2', teams: 1},
+    {date: '2015-03-24', title: 'Sweet 16: Day 1', teams: 1},
+    {date: '2015-03-25', title: 'Sweet 16: Day 2', teams: 1},
+    {date: '2015-03-26', title: 'Elite 8: Day 1', teams: 1},
+    {date: '2015-03-27', title: 'Elite 8: Day 2', teams: 1},
+    {date: '2015-04-02', title: 'Final Four', teams: 1},
+    {date: '2015-04-04', title: 'Championship', teams: 1}
+  ];
+  res.send(dates);
 });
 
 
