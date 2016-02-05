4d84ec514cbf52b257ab6dd91917c3d6c0cae367
Kevin Kerr <kkerr1@gmail.com>
2016-02-04 22:21:54 -0800

    get eslint running on server.

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -14,7 +14,7 @@ router.use(function timeLog(req, res, next) {
 router.get('/dates', function (req, res) {
 
   var dates = [
-    {date: '2015-03-17', title: 'Round of 64: Day 1', teams: 2},
+    {date: '2015-03-17', title: 'Round of 64: Day 1', teams: 2, current: true},
     {date: '2015-03-18', title: 'Round of 64: Day 2', teams: 2},
     {date: '2015-03-19', title: 'Round of 32: Day 1', teams: 1},
     {date: '2015-03-20', title: 'Round of 32: Day 2', teams: 1},
