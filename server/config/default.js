9dc3b54f366f45f930258957f896830fc0c4871a
Kevin Kerr <kkerr1@gmail.com>
2016-02-27 14:55:24 -0800

    switch port / host config to use config module

--- server/config/default.js
+++ server/config/default.js
@@ -4,6 +4,8 @@
 
 module.exports = {
   server: {
+    port: 3000,
+    host: 'localhost',
     staticFiles: {
       ttlMilliseconds: 60 * 60 * 24 * 1000
     }
--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,10 @@
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
 }
-var defaultPort = 3000;
-var HOST = process.env.HOST || '127.0.0.1';
-var PORT = process.env.PORT || defaultPort;
+var config = require('config');
+
+var HOST = config.get('server.host');
+var PORT = config.get('server.port');
 
 var express = require('express');
 var api = require('./routes/api');
